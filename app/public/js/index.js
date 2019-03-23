c4243ff4e41075739cdc1d3814b5180c8491b44f
Rhubarb <edlun066@d.umn.edu>
2019-03-23 13:49:28 -0500

    Chirpy Docs

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -97,3 +97,60 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+$.get("/api/all", function(data) {
+
+  if (data.length !== 0) {
+
+    for (var i = 0; i < data.length; i++) {
+
+      var row = $("<div>");
+      row.addClass("chirp");
+
+      row.append("<p>" + data[i].author + " chirped.. </p>");
+      row.append("<p>" + data[i].body + "</p>");
+      row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
+
+      $("#chirp-area").prepend(row);
+
+    }
+
+  }
+
+});
+
+////// Chirp
+
+// When user chirps (clicks addBtn)
+$("#chirp-submit").on("click", function(event) {
+  event.preventDefault();
+
+  // Make a newChirp object
+  var newChirp = {
+    author: $("#author").val().trim(),
+    body: $("#chirp-box").val().trim(),
+    created_at: moment().format("YYYY-MM-DD HH:mm:ss")
+  };
+
+  console.log(newChirp);
+
+  // Send an AJAX POST-request with jQuery
+  $.post("/api/new", newChirp)
+    // On success, run the following code
+    .then(function() {
+
+      var row = $("<div>");
+      row.addClass("chirp");
+
+      row.append("<p>" + newChirp.author + " chirped: </p>");
+      row.append("<p>" + newChirp.body + "</p>");
+      row.append("<p>At " + moment(newChirp.created_at).format("h:mma on dddd") + "</p>");
+
+      $("#chirp-area").prepend(row);
+
+    });
+
+  // Empty each input box by replacing the value with an empty string
+  $("#author").val("");
+  $("#chirp-box").val("");
+});
