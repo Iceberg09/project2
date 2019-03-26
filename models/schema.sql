7dbda23242b3d614b1c28b53773c461181029906
Jacob Dahlberg <dahlberg2009@gmail.com>
2019-03-25 20:42:31 -0500

    Adding database and table creation along with initial car make/model data

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,30 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS project2_db;
+CREATE DATABASE project2_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE project2_db;
+
+CREATE TABLE cars (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    make VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    year INT
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    user_access VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE reviews (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    make VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    color VARCHAR(50),
+    license_plate VARCHAR(50),
+    rating INT,
+    comment VARCHAR(300),
+    date_time DATETIME,
+    location VARCHAR(100)
+);
