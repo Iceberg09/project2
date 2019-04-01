fb11bbb3202fd41abc6f7b9a3d6a177cd9d1161b
Jacob Dahlberg <jdahlberg@peoplenetonline.com>
2019-04-01 16:07:50 -0500

    Adding initial apiroutes

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -2,9 +2,44 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/cars/:make", function(req, res) {
+    db.car.findAll({
+      where: {
+        make: req.params.make
+      }
+    }).then(function(dbModels) {
+      res.json(dbModels);
+    });
+  });
+
+  app.get("/api/cars/:model", function(req, res) {
+    db.car.findAll({
+      where: {
+        model: req.params.model
+      }
+    }).then(function(dbModels) {
+      res.json(dbModels);
+    });
+  });
+
+  app.get("/api/reviews/:lp", function(req, res) {
+    db.review.findAll({
+      where: {
+        license_plate: req.params.lp
+      }
+    }).then(function(dbReviews) {
+      res.json(dbReviews);
+    });
+  });
+
+  app.get("api/users/:email/:password", function(req,res) {
+    db.user.findOne({
+      where: {
+        email: req.params.email,
+        password: req.params.password
+      }
+    }).then(function(dbUser) {
+      res.json(dbUser);
     });
   });
 
@@ -19,36 +54,6 @@ module.exports = function(app) {
   app.delete("/api/examples/:id", function(req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
-    });
-  });
-};
-
-var connection = require("../config/connection.js");
-
-// Chirpy Routes
-// =============================================================
-module.exports = function(app) {
-  // Get all chirps
-  app.get("/api/all", function(req, res) {
-    var dbQuery = "SELECT * FROM chirps";
-
-    connection.query(dbQuery, function(err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
-  });
-
-  // Add a chirp
-  app.post("/api/new", function(req, res) {
-    console.log("Chirp Data:");
-    console.log(req.body);
-
-    var dbQuery = "INSERT INTO chirps (author, body, created_at) VALUES (?,?,?)";
-
-    connection.query(dbQuery, [req.body.author, req.body.body, req.body.created_at], function(err, result) {
-      if (err) throw err;
-      console.log("Chirp Successfully Saved!");
-      res.end();
     });
   });
 };
