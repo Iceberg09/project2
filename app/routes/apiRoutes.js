7b81a2fdfbc4c51fc32668cb43cd143ba0376083
Jacob Dahlberg <dahlberg2009@gmail.com>
2019-04-01 21:10:55 -0500

    user login and create account sequelize complete

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,39 +1,39 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/cars/:make", function(req, res) {
-    db.car.findAll({
+  app.get("/api/cars/:make", function (req, res) {
+    db.cars.findAll({
       where: {
         make: req.params.make
       }
-    }).then(function(dbModels) {
+    }).then(function (dbModels) {
       res.json(dbModels);
     });
   });
 
-  app.get("/api/cars/:model", function(req, res) {
-    db.car.findAll({
+  app.get("/api/cars/:model", function (req, res) {
+    db.cars.findAll({
       where: {
         model: req.params.model
       }
-    }).then(function(dbModels) {
+    }).then(function (dbModels) {
       res.json(dbModels);
     });
   });
 
-  app.get("/api/reviews/:lp", function(req, res) {
-    db.review.findAll({
+  app.get("/api/reviews/:lp", function (req, res) {
+    db.reviews.findAll({
       where: {
         license_plate: req.params.lp
       }
-    }).then(function(dbReviews) {
+    }).then(function (dbReviews) {
       res.json(dbReviews);
     });
   });
 
-  app.get("api/users/:email/:password", function(req,res) {
-    db.user.findOne({
+  app.get("/api/users/:email/:password", function (req, res) {
+    db.user.findAll({
       where: {
         email: req.params.email,
         password: req.params.password
@@ -43,17 +43,15 @@ module.exports = function(app) {
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  app.post("/api/users", function(req, res) {
+    db.user.create({
+      email: req.body.email,
+      password: req.body.password,
+      user_acces: req.body.userAccess
+    }).then(function(dbUser) {
+      res.json(dbUser);
+    }).catch(function(err) {
+      res.json(err);
+    })
+  })
 };
