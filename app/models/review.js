699ebe409b22385e8914d320d20ac1dfa5026fb8
Kenny <nels5410@gmail.com>
2019-04-02 18:51:12 -0500

    New Review API
    Completed the API route for a new review to be submitted from the new review form.

--- app/models/review.js
+++ app/models/review.js
@@ -1,5 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   var review = sequelize.define("review", {
+    name: DataTypes.STRING,
     make: DataTypes.STRING,
     model: DataTypes.STRING,
     color: DataTypes.STRING,
